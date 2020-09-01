f86beafc763ec1534fe5778425d33a9eadd1b68d
andratarau5 <andra.tarau96@gmail.com>
2020-09-01 18:48:22 +0300

    new design

--- src/main/java/com/roxan/bookreward/controller/BookController.java
+++ src/main/java/com/roxan/bookreward/controller/BookController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
@@ -17,25 +18,20 @@ public class BookController {
     @Autowired
     private BookRepository bookRepository;
 
-    @GetMapping("/allbooks")
+    @GetMapping("/showallbooks")
     public String showAllBooks(Model model){
         List<Book> bookList = bookRepository.findAll();
         model.addAttribute("books",bookList);
         return "showallbooks";
     }
 
-//    @PostMapping("/allbooks")
-//    public String showAllBooks(@ModelAttribute Book book){
-//        bookRepository.findAll();
-//        return "redirect:/allbooks";
-//    }
 
     @GetMapping("/")
     public String index(){
         return "index";
     }
 
-    @GetMapping("/addbook")
+    @GetMapping("/addbook")              // we need GetMapping so we can populate our template
     public String addBook(Model model){
         model.addAttribute("book",new Book());
         return "addbook";
@@ -44,6 +40,30 @@ public class BookController {
     @PostMapping("/addbook")
     public String addBook(@ModelAttribute Book book){
         bookRepository.save(book);
-        return "redirect:/allbooks";
+        return "redirect:/showallbooks";
+    }
+
+    @GetMapping("/editbook/{id}")
+    public String editBook(Model model, @PathVariable Integer id){
+        Book book = bookRepository.findById(id).get();
+        model.addAttribute("book",new Book());
+        return "editbook";
+    }
+
+    @PostMapping("/editbook/{id}")
+    public String editBook(@ModelAttribute Book book, @PathVariable Integer id){
+        Book database_book = bookRepository.findById(id).get(); //get it from the database to be able to update the id
+        database_book.setTitle(book.getTitle());    //update fields
+        database_book.setAuthor(book.getAuthor());
+        database_book.setYear(book.getYear());
+        System.out.println(database_book);
+        bookRepository.save(book);
+        return "redirect:/showallbooks";
+    }
+
+    @GetMapping("/deletebook/{id}")
+    public String deleteBook(@PathVariable Integer id){
+        bookRepository.deleteById(id);
+        return "redirect:/showallbooks";
     }
 }
